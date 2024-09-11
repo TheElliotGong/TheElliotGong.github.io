7f592e54f2cbf609d6643e38dc7304a868335d30
thetyper101 <ewcgong@hotmail.com>
2024-09-11 16:44:01 -0700

    Revised resumes and attempted implementation of react

---
+++ src/components.jsx
@@ -0,0 +1,21 @@
+
+
+
+const project = (props) => {
+    return <div className ="project column is-full-mobile is-half-tablet is-one-third-desktop">
+                        <h4 className="has-text-weight-bold is-size-4">Halo Energy Sword(2021)</h4>
+                        <div className="sketchfab-embed-wrapper"> <iframe title="Halo Energy Sword" frameborder="0"
+                                allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true"
+                                allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking
+                                execution-while-out-of-viewport execution-while-not-rendered web-share
+                                src="https://sketchfab.com/models/3f4cf22e9c624ad588a93bd4f307ff11/embed" width="100%"
+                                height="300px"> </iframe> </div>
+                        <ul>
+                            <li><strong>About:</strong> Modelled a low poly energy sword from Halo.
+                            </li>
+                            <li><strong>Team Size:</strong> Self</li>
+                            <li><strong>Project Length:</strong> 1 week</li>
+                            <li><strong>Tools:</strong> Maya, Substance Painter</li>
+                        </ul>
+                    </div>
+}
