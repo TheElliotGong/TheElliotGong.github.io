1ca11105494bf7573f0a12904f6d82d1ea13de1e
thetyper101 <ewcgong@hotmail.com>
2024-09-28 23:10:08 -0700

    Added some more libraries to help with the react conversion

--- src/components.jsx
+++ src/components.jsx
@@ -3,19 +3,13 @@
 
 const project = (props) => {
     return <div className ="project column is-full-mobile is-half-tablet is-one-third-desktop">
-                        <h4 className="has-text-weight-bold is-size-4">Halo Energy Sword(2021)</h4>
-                        <div className="sketchfab-embed-wrapper"> <iframe title="Halo Energy Sword" frameborder="0"
-                                allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true"
-                                allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking
-                                execution-while-out-of-viewport execution-while-not-rendered web-share
-                                src="https://sketchfab.com/models/3f4cf22e9c624ad588a93bd4f307ff11/embed" width="100%"
-                                height="300px"> </iframe> </div>
+                        <h4 className="has-text-weight-bold is-size-4">props.name</h4>
+                        <div className="sketchfab-embed-wrapper">  </div>
                         <ul>
-                            <li><strong>About:</strong> Modelled a low poly energy sword from Halo.
-                            </li>
-                            <li><strong>Team Size:</strong> Self</li>
-                            <li><strong>Project Length:</strong> 1 week</li>
-                            <li><strong>Tools:</strong> Maya, Substance Painter</li>
+                            <li><strong>About:</strong>props.description</li>
+                            <li><strong>Team Size:</strong>props.teamCount</li>
+                            <li><strong>Project Length:</strong>props.length</li>
+                            <li><strong>Tools:</strong>props.tools</li>
                         </ul>
                     </div>
 }
