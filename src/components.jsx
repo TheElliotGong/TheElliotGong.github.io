a1755b9bbe764ef685c910ca85ff27370c7cce79
thetyper101 <ewcgong@hotmail.com>
2025-04-22 10:51:49 -0700

    Attempt to use carousel to display projects

--- src/components.jsx
+++ src/components.jsx
@@ -2,14 +2,20 @@
 
 
 const project = (props) => {
-    return <div className ="project column is-full-mobile is-half-tablet is-one-third-desktop">
-                        <h4 className="has-text-weight-bold is-size-4">props.name</h4>
-                        <div className="sketchfab-embed-wrapper">  </div>
+    <div class="project column is-full-mobile is-half-tablet is-one-third-desktop">
+                        <h4 class="has-text-weight-bold is-size-4">{props.name}</h4>
+
+                        <a href={props.url} target="_blank">
+                            <img src={props.img} alt=""></img>
+                        </a>
                         <ul>
-                            <li><strong>About:</strong>props.description</li>
-                            <li><strong>Team Size:</strong>props.teamCount</li>
-                            <li><strong>Project Length:</strong>props.length</li>
-                            <li><strong>Tools:</strong>props.tools</li>
+                            <li><strong>Link:</strong> <a href="https://www.artstation.com/artwork/8b2D6O"
+                                    target="_blank"> {props.linkDescription}</a></li>
+                            <li><strong>About:</strong> {props.projectDescription}
+                            </li>
+                            <li><strong>Team Size:</strong> {props.teamSize}</li>
+                            <li><strong>Project Length:</strong> {props.projectLength}</li>
+                            <li><strong>Tools:</strong> {props.tools}</li>
                         </ul>
                     </div>
 }
